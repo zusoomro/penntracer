4f65850e046bf351d6a3d8e98e1dcad7ff7d3271
zusoomro <zusoomro@seas.upenn.edu>
2020-08-17 12:33:36 -0400

    Updates

---
+++ precache-manifest.ff328b0b8ef4701ea78da9f5dfc926ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945a1242fc8a1a1f02cab3c96efdc498",
+    "url": "/penntracer/index.html"
+  },
+  {
+    "revision": "dbe50354d88c9df90524",
+    "url": "/penntracer/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "50eac1744f1335f053ad",
+    "url": "/penntracer/static/js/2.96e5e7e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/penntracer/static/js/2.96e5e7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe50354d88c9df90524",
+    "url": "/penntracer/static/js/main.9bb33f79.chunk.js"
+  },
+  {
+    "revision": "755fe3875fb13a30cbb8",
+    "url": "/penntracer/static/js/runtime-main.63709f48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/penntracer/static/media/logo.5d5d9eef.svg"
+  }
+]);
